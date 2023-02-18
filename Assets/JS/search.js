8749d84d961bdb3c31a6d3a7a9ec333b42e450da
MikeTheDevWasNotHere <124840291+MikeTheDevWasNotHere@users.noreply.github.com>
2023-02-18 13:50:59 -0500

    Added and Finalized Buttons

--- Assets/JS/search.js
+++ Assets/JS/search.js
@@ -1,9 +1,10 @@
 let jsonData = `[
     {
         "Name": "Crossy Road",
-	"Type": "Arcade",
-	"Preview": "https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2?mode=scale&q=90&h=400&w=800&background=%230078D7",
-	"Open": "NA",
+        "Type": "Arcade",
+        "Preview": "https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2?mode=scale&q=90&h=400&w=800&background=%230078D7",
+        "Play": "index.html",
+        "Open": "index.html",
         "Date": "2/17/23",
         "Time": "11:53 AM",
         "Status": "C0",
@@ -11,9 +12,10 @@ let jsonData = `[
     },
     {
         "Name": "Tower Blocks",
-	"Type": "Strategy",
-	"Preview": "https://androidcommunity.com/wp-content/uploads/2016/02/featured-stack.jpg",
-	"Open": "NA",
+        "Type": "Strategy",
+        "Preview": "https://androidcommunity.com/wp-content/uploads/2016/02/featured-stack.jpg",
+        "Play": "index.html",
+        "Open": "index.html",
         "Date": "2/17/23",
         "Time": "11:54 AM",
         "Status": "C0",
@@ -33,13 +35,14 @@ function search_jsonData() {
         let obj = data[i];
 
         if (
-            obj.Name.toLowerCase().includes(input)   || 
-            obj.Type.toLowerCase().includes(input)   || 
-            obj.Time.toLowerCase().includes(input)   || 
-            obj.Date.toLowerCase().includes(input)   ||
+            obj.Name.toLowerCase().includes(input) ||
+            obj.Type.toLowerCase().includes(input) ||
+            obj.Time.toLowerCase().includes(input) ||
+            obj.Date.toLowerCase().includes(input) ||
             obj.Status.toLowerCase().includes(input)
         ) {
             const elem = document.createElement("p")
+
             elem.innerHTML = `
                 <div class='bg-white p-8 rounded-lg shadow-md flex max-w-lg'>
                     <div class='w-1/2'>
@@ -50,13 +53,13 @@ function search_jsonData() {
                         <p class='text-gray-600 text-center'>${obj.Status} <span class='text-gray-400 block text-xs'>Status</span></p>
                     </div>
 
-                        <button class='py-1 text-sm px-6 bg-green-500 text-white rounded'>Play</button>
+                    <a href="${obj.Play}"><button class='py-1 text-sm px-6 bg-green-500 text-white rounded'>Play</button></a>
+                    <a href="${obj.Open}"><button class='py-1 text-sm px-6 bg-blue-500 text-white rounded'>Open</button></a>
                     </div>
 
                         <img class='w-1/2 h-auto rounded' src='${obj.Preview}' alt=''>
                     </div>
-		    
-		    <br>
+		        <br>
                 `
             x.appendChild(elem)
         }
