4386f5a10be16c96c80978e2b0b07919238a52ba
MikeTheDevWasNotHere <124840291+MikeTheDevWasNotHere@users.noreply.github.com>
2023-02-23 14:18:18 -0500

    Test Screen Size Display

---
+++ Assets/JS/device.js
@@ -0,0 +1,7 @@
+if (screen.width <= 700) {
+    document.location = "../../MikeTheDevWasNotHere/TR_Gamevity/Devices/Mobile/index.html";
+}
+
+else {
+    document.location = "../../MikeTheDevWasNotHere/TR_Gamevity/Devices/Computer/index.html";
+}
